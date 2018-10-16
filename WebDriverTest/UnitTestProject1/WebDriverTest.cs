27383bdb883ea312c1162ddf9513f9b356ffc258
Alexei <alvo1998@mail.ru>
2018-10-16 16:53:48 +0300

    commit laba4 ( WebDriverTest )

---
+++ WebDriverTest/UnitTestProject1/WebDriverTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class WebDriverTest
+    {
+        [TestMethod]
+        public void inability_to_order_past_date()
+        {
+            IWebDriver Driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl("http://topavia.by/");
+
+            Driver.FindElement(By.ClassName("svg-search")).Click();
+            Driver.FindElement(By.Id("title-search-input")).SendKeys("Минск");
+            Driver.FindElement(By.ClassName("svg-black")).Click();
+            Driver.Quit();
+        }
+    }
+}
