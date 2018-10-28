63b2348a000c33951524a710a41071e67893bc47
Alexei <alvo1998@mail.ru>
2018-10-28 18:00:13 +0300

    Add laba InitTestProject whits use PageObject and PageFactory

---
+++ WebDriverTest_PageObject/UnitTestProject2/PageObject_WebDriverTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjectPattern
+{
+    public class PageObject_WebDriverTest
+    {
+        private readonly IWebDriver driver;
+        private readonly string url = @"http://topavia.by/";
+
+        public PageObject_WebDriverTest(IWebDriver browser)
+        {
+            this.driver = browser;
+            this.driver.Manage().Window.Maximize();
+            PageFactory.InitElements(browser, this);
+        }
+
+        [FindsBy(How = How.ClassName, Using = "sendpulse-disallow-btn")]
+        public IWebElement Chto_mashaet { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "nemo-ui-dummiedInput__dummy")]
+        public IWebElement Otkyda_letim { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "nemo-ui-textInput__input")]
+        public IWebElement Otkyda_letim_text { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "js-autofocus-field_arrival")]
+        public IWebElement Kyda_letim_text { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "js-autofocus-field_date")]
+        public IWebElement Date { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "nemo-flights-form__searchButton")]
+        public IWebElement Knopka_Poisk { get; set; }
+
+        public void Navigate()
+        {
+            this.driver.Navigate().GoToUrl(this.url);
+        }
+    }
+}
---
+++ WebDriverTest_PageObject/UnitTestProject2/WebDriverTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PageObjectPattern
+{
+    [TestClass]
+    public class BingTests
+    {
+        public IWebDriver Driver { get; set; }
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            this.Driver = new FirefoxDriver();
+        }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            this.Driver.Quit();
+        }
+
+        [TestMethod]
+        public void SearchTextInBing()
+        {
+            PageObject_WebDriverTest googleMainPage = new PageObject_WebDriverTest(this.Driver);
+            googleMainPage.Navigate();
+            System.Threading.Thread.Sleep(5000);
+
+            googleMainPage.Chto_mashaet.Click();
+
+            googleMainPage.Otkyda_letim.Click();
+            googleMainPage.Otkyda_letim_text.SendKeys("Пекин,");
+            System.Threading.Thread.Sleep(2000);
+
+            googleMainPage.Kyda_letim_text.SendKeys("");
+            System.Threading.Thread.Sleep(2000);
+
+            googleMainPage.Date.SendKeys("21.11.2018");
+            googleMainPage.Date.SendKeys(Keys.Enter);
+            System.Threading.Thread.Sleep(2000);
+
+            googleMainPage.Knopka_Poisk.Click();
+
+            Assert.AreEqual(Driver.FindElement(By.ClassName("nemo-flights-form__route__segment__item__error")).Text, "Укажите пункт прилета", "No point of arrival indicated");
+        }
+    }
+}
